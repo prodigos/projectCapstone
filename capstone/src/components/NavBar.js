158f301029afbb98c28f3172a35c4af0d5967dbd
prodigos <wilghensantos@gmail.com>
2021-01-12 12:26:01 -0500

    created react components

---
+++ capstone/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const NavBar = () => {
+
+    return (
+        <div>
+            <h1>Ask Will.</h1>
+        </div>
+    )
+
+}
+
+export default NavBar;
---
+++ capstone/src/components/QuestionsDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuestionsDisplay = () => {
+
+    return (
+        <div>
+            <h1>display questions here</h1>
+        </div>
+    )
+}
+
+export default QuestionsDisplay;
